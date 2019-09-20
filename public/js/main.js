a98be7ef0707b6e60a8654ad8fc6e9801106a45b
williamestrada <willy.estrada81@gmail.com>
2019-09-20 17:07:28 -0400

    Landing Page and Index page Styling

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,75 @@
-function DisplayTime() {
-    
-        var dt = new Date();
-document.getElementById("curTime").innerHTML = dt.toLocaleTimeString();
 
+
+const time = document.getElementById("curTime"),
+      greeting = document.getElementById("greeting");
+
+
+
+function DisplayTime() {
+    let date = new Date();
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    //SET AM OR PM
+    const amPM = hour >= 12 ? "PM" : "AM";
+
+    //12hr Format
+
+    hour = hour % 12 || 12;
+
+    //Output time
+
+    time.textContent =`${hour}:${addZero(minutes)}:${addZero(seconds)}`;
+
+    setTimeout(DisplayTime, 1000);
 }
-window.onload = DisplayTime
+
+//Add Zeros to the time
+
+function addZero(n) {
+        return (parseInt(n, 10) < 10 ? "0" : "" ) + n;
+}
+function bgMorning() {
+        var random = Math.floor(Math.random() * 4) + 0;
+        var img = ["url('../img/bg-images/morning-1.jpg')",
+                "url('../img/bg-images/morning-2.jpg')",
+                "url('../img/bg-images/morning-3.jpg')",
+                "url('../img/bg-images/morning-4.jpg')"];
+        
+        document.body.style.backgroundImage = img[random];
+}
+function bgAfternoon() {
+        var random = Math.floor(Math.random() * 4) + 0;
+        var img = ["url('../img/bg-images/afternoon-1.jpg')",
+                "url('../img/bg-images/afternoon-2.jpg')",
+                "url('../img/bg-images/afternoon-3.jpg')",
+                "url('../img/bg-images/afternoon-4.jpg')"];
+        document.body.style.backgroundImage = img[random];
+}
+function bgEvening() {
+        var random = Math.floor(Math.random() * 4) + 0;
+        var img = ["url('../img/bg-images/night-1.jpg')",
+                "url('../img/bg-images/night-2.jpg')",
+                "url('../img/bg-images/night-3.jpg')",
+                "url('../img/bg-images/night-4.jpg')"];
+        
+        document.body.style.backgroundImage = img[random];
+       
+}
+function displayGreeting (){
+        let date = new Date();
+        let hour = date.getHours();
+        if (hour < 12 ){
+                greeting.textContent = "Good Morning";
+                bgMorning();
+        } else if(hour < 18) {
+                greeting.textContent = "Good Afternoon";
+                bgAfternoon();
+        } else {
+                greeting.textContent = "Good Evening";
+                bgEvening();
+        }
+}
+DisplayTime();
+displayGreeting();
--- routes/index.js
+++ routes/index.js
@@ -15,44 +15,50 @@ router.post('/', (req, res) => {
     axios.get(api)
         .then(response => {
             let err = response.status;
-            console.log(err)
+            if (response.status == 200) {
+                function toFahrenheit(kelvin) {
+                    let fahrenheit = ((kelvin - 273.15) * 1.8) + 32;
+                    return fahrenheit.toFixed(2);
+                }
+                // let city = response.data.city[0].name;
+                let temp = toFahrenheit(response.data.list[0].main.temp)
+                // console.log(temp)
+                let weather = response.data.list[0].weather[0].main;
+                // console.log(weather)
+                let temp_min = toFahrenheit(response.data.list[0].main.temp_min);
+                // console.log(temp_min)
+                let temp_max = toFahrenheit(response.data.list[0].main.temp_max);
+                let wind = response.data.list[0].wind.speed;
+                // console.log(temp_max)
 
-            function toFahrenheit(kelvin) {
-                let fahrenheit = ((kelvin - 273.15) * 1.8) + 32;
-                return fahrenheit.toFixed(2);
+
+                let weatherData = [
+                    {
+                        city: cityName,
+                        temp: temp,
+                        temp_min: temp_min,
+                        temp_max: temp_max,
+                        wind: wind,
+                        weather: weather
+                    }
+                ]
+                res.render("weather", {
+                    weatherData: weatherData
+
+                })
+                console.log(weatherData);
+
+            } else {
+                errors.push["Please, check the city spelling and try again"]
+                res.render("index"
+                )
             }
-            // let city = response.data.city[0].name;
-            let temp = toFahrenheit(response.data.list[0].main.temp)
-            // console.log(temp)
-            let weather = response.data.list[0].weather[0].main;
-            // console.log(weather)
-            let temp_min = toFahrenheit(response.data.list[0].main.temp_min);
-            // console.log(temp_min)
-            let temp_max = toFahrenheit(response.data.list[0].main.temp_max);
-            let wind = response.data.list[0].wind.speed;
-            // console.log(temp_max)
 
             
-            let weatherData = [
-                {
-                    
-                    temp: temp,
-                    temp_min: temp_min,
-                    temp_max: temp_max,
-                    wind: wind,
-                    weather: weather
-                }
-            ]
-            res.render("weather", {
-                weatherData: weatherData
-                
-            })
-            console.log(weatherData);
-
         })
         .catch(error => {
             
-            console.log(err);
+            console.log(error);
             if (error) {
                 errors.push["Please, check the city spelling and try again"]
                 res.render("index"
